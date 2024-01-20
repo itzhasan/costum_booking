3ebd91263ddad42b82732cbd707f784d252f4247
noorwaleed01 <114314289+noorwaleed01@users.noreply.github.com>
2024-01-20 19:59:31 +0300

    complete some of create

--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,14 +1,19 @@
 //main function check if user have account or not
 import {Login} from "./auth/login"
+
 import { MainPage } from "./home/main"
 import { Dashboard } from "./dashboard/dashboard"
 import { CreateNew } from "./createNew/createNew"
 import { BusinessDescribtion } from "./createNew/businessDescribtion"
 import { ServiceMenu } from "./createNew/serviceMenu"
+import { Signup } from "./auth/signup" 
+
 export const Index =() =>{
     return(
         <div>
-            <ServiceMenu/>
+            <Signup/>
+
         </div>
     )
 }
+
