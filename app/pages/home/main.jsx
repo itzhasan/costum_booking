abb149be1553f38e73c64692d101ce135da7571d
noorwaleed01 <114314289+noorwaleed01@users.noreply.github.com>
2024-01-21 20:53:36 +0300

    complete footer

--- app/pages/home/main.jsx
+++ app/pages/home/main.jsx
@@ -1,10 +1,12 @@
 import { MainHeader } from "@/app/components/home/mainHeader"
 import { MainBody } from "@/app/components/home/mainBody"
+// import { MainFooter } from "@/app/components/home/mainFooter"
 export const MainPage = () => {
     return(
         <div>
             <MainHeader/>
             <MainBody/>
+            {/* <MainFooter/> */}
             
 
 
--- app/pages/index.jsx
+++ app/pages/index.jsx
@@ -1,6 +1,5 @@
 //main function check if user have account or not
 import {Login} from "./auth/login"
-
 import { MainPage } from "./home/main"
 import { Dashboard } from "./dashboard/dashboard"
 import { CreateNew } from "./createNew/createNew"
@@ -11,7 +10,7 @@ import { Signup } from "./auth/signup"
 export const Index =() =>{
     return(
         <div>
-            <Signup/>
+            <BusinessDescribtion/>
 
         </div>
     )
